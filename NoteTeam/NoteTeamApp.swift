13e4f9f2b37a62933905ea5fbd7ef9210f3dd588
norahm224 <norah.213113778@gmail.com>
2023-01-08 11:09:41 +0300

    Initial Commit

---
+++ NoteTeam/NoteTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTeamApp.swift
+//  NoteTeam
+//
+//  Created by Nourah Almusaad on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NoteTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
